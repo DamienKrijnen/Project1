3f289921c2c145dc4275ee5a2ef87659c8dacb6f
LBC187-winniedos <9002353@student.tcrmbo.nl>
2025-09-02 15:37:54 +0200

    added index styling and basic layout

---
+++ Project1/boilerplate-theme/functions.php
@@ -0,0 +1,4 @@
+<?php
+// import main, and 
+wp_enqueue_style("main-css", get_template_directory_uri() . '/css/main.css');
+wp_enqueue_style("footer-css", get_template_directory_uri() . '/css/footer.css');
---
+++ functions.php
@@ -0,0 +1,5 @@
+<?php
+// import main, and load main css
+wp_enqueue_style( "/style.css", get_template_directory_uri() . '/css/main.css'); 
+?>
+
