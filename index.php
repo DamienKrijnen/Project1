493b2f37b553ff85981e16a6c8d7960c33225257
LBC187-winniedos <9002353@student.tcrmbo.nl>
2025-09-03 14:56:57 +0200

    Mobile view is done

--- index.php
+++ index.php
@@ -3,19 +3,19 @@ get_header();
 
 ?>
 <main>
-    <section class="image-center-large container text-center">
+    <section class="image-center-large">
         <!-- background using get_template_directory_uri specific wordpress function to define image source -->
-        <img  class="col-6 col-md-4" src="<?php echo get_template_directory_uri() . './assets/images/background_restaurant.png'; ?>" alt="achtergrond van een restaurant">
+        <img src="<?php echo get_template_directory_uri() . './assets/images/background_restaurant.png'; ?>" alt="achtergrond van een restaurant">
     </section>
 
 
-    <section class="general-info coll-auto" >
-        <div class="general-info-text col-6 col-md-4">
+    <section class="general-info">
+        <div class="general-info-text">
             <h1>Welkom bij xxxxxx</h1>
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae quaerat a commodi error rerum suscipit qui deserunt officia magni ut, nemo veniam, sunt, totam alias voluptatum. Unde voluptas voluptatem perferendis.</p>
             <button class="general-info-button"><b>Contact</b></button>
         </div>
-        <div class="general-info-img col-6 col-md-4">
+        <div class="general-info-img">
             <img src="<?php echo get_template_directory_uri() . './assets/images/company_ceo.png'; ?>" alt="ceo and owner of the company Lucy">
             <img src="<?php echo get_template_directory_uri() . './assets/images/company_ceo.png'; ?>" alt="ceo and owner of the company Lucy">
         </div>
@@ -29,7 +29,7 @@ get_header();
         <button class="menuCard-button"><a href="<?php echo get_site_url() . '#'; ?>"> Download in PDF</a></button>
         <div class="menuCard-reservation-background">
             <h1>Reserveer</h1>
-            <button class="menuCard-reservation-button"><a href="<?php echo get_site_url() . '#'; ?>"> 0118- 00 11 22 </a></button>
+            <!-- <button class="menuCard-reservation-button"><a href="<?php echo get_site_url() . '#'; ?>"> 0118- 00 11 22 </a></button> -->
         </div>
     </section>
     <!-- linking using get_site_url is a  specific wordpress function to define a URL source -->
